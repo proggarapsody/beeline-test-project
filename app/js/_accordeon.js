4632cafef3ca5af366de0977b430a66a87b9c4f9
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-05-17 17:59:53 +0300

    feat: dist

--- app/js/_accordeon.js
+++
@@ -1,8 +0,0 @@
-const accordions = document.querySelectorAll('.sidebar__accordeon-header');
-
-accordions.forEach((el) => {
-  el.addEventListener('click', () => {
-    el.classList.toggle('active');
-    el.parentElement.lastElementChild.classList.toggle('visible');
-  });
-});
--- app/js/app.js
+++
@@ -1,8 +0,0 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
-import './_swiper.js';
-import './_accordeon.js';
-import './_sidebar.js';
-document.addEventListener('DOMContentLoaded', () => {
-  // Custom JS
-});
